b7f3ac3b239ecd7c5c73746176b59671a88ba370
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-05-03 08:29:37 +0300

    Vider la corbeille

--- data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE vegetable (
-    id_vegetable INTEGER PRIMARY KEY AUTO_INCREMENT,
-    vegetable_name VARCHAR(40)
-);
-
-INSERT INTO vegetable (vegetable_name) VALUES
-('Anana'),
-('Voanjobory'),
-('Ovy');
